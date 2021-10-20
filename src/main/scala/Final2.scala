75c0d129e70054b72bc41743d3131eb57765d00c
DaceM <dacemarkevica@gmail.com>
2021-10-20 17:37:25 +0300

    Update1

--- src/main/scala/Final2.scala
+++ src/main/scala/Final2.scala
@@ -1,103 +1,103 @@
 object Final2 extends App {
+
   import scala.io.StdIn.readLine
 
-   //assignment of global variables
-    var player1 = " Player 1"
-    var player2 = "Player 2"
-    var whoseTurn = "O"
-    var winner = " "
-    var gameOver = false
-    var grid: Array[Array[String]] = Array(Array(" ", " ", " "), Array(" ", " ", " "), Array(" ", " ", " "))
-    var startNewGame = true
+  //assignment of global variables
+  var player1 = " Player 1"
+  var player2 = "Player 2"
+  var whoseTurn = "O"
+  var winner = " "
+  var gameOver = false
+  var grid: Array[Array[String]] = Array(Array(" ", " ", " "), Array(" ", " ", " "), Array(" ", " ", " "))
+  var startNewGame = true
 
-    def mainGameLoop(): Unit = {
-      printInstructions()
-      while (startNewGame) {
-        singleGameLoop()
-        val response = readLine("New game Y/N ?")
-        if (response.toLowerCase.startsWith("y")) {
-          resetGameState()
-        } else startNewGame = false
+  def mainGameLoop(): Unit = {
+    printInstructions()
+    while (startNewGame) {
+      singleGameLoop()
+      val response = readLine("New game Y/N ?")
+      if (response.toLowerCase.startsWith("y")) {
+        resetGameState()
+      } else startNewGame = false
+    }
+  }
+
+  mainGameLoop()
+
+  def singleGameLoop(): Unit = {
+    //loop that continues the game while the game is not over
+    while (!gameOver) {
+      printGrid(grid)
+      playerMove(whoseTurn, grid)
+    }
+  }
+
+  def resetGameState() = {
+    grid = Array(Array(" ", " ", " "), Array(" ", " ", " "), Array(" ", " ", " "))
+    whoseTurn = "O"
+    winner = " "
+    gameOver = false
+  }
+
+  //This function prints out instructions at the beginning
+  def printInstructions(): Unit = {
+    println("INSTRUCTIONS:")
+    println("The grid is of the form")
+    println("|-----|-----|-----|")
+    for (i <- 0 to 2; j <- 0 to 2) {
+      if (j == 0) {
+        print("|")
+      }
+      print(s"($i,$j)")
+      if (j == 2) {
+        println("|")
+        println("|-----|-----|-----|")
+      }
+      else {
+        print("|")
       }
     }
-    mainGameLoop()
+    println("To place your symbol, you must enter the coordinate where you wish to place your symbol.")
+    println("Input must be given in the form x,y with no space or brackets.")
+    println("Example: Where would you like to place your O? 1,1")
+    println("In case of incorrect placement, the system will ask you to try again.")
+    println("That's all. Have fun!")
+    println()
+    println("GAME BEGINS")
+  }
 
-    def singleGameLoop(): Unit = {
-      //loop that continues the game while the game is not over
-      while (!gameOver) {
-        printGrid(grid)
-        playerMove(whoseTurn, grid)
+  //this function prints the grid after every move
+  def printGrid(grid: Array[Array[String]]): Unit = {
+    println("|---|---|---|")
+    for (i <- 0 to 2; j <- 0 to 2) {
+      if (j == 0) {
+        print("| ")
+      }
+      print(grid(i)(j))
+      if (j == 2) {
+        println(" | ")
+        println("|---|---|---|")
+      }
+      else {
+        print(" | ")
       }
     }
+  }
 
-    def resetGameState() = {
-      grid = Array(Array(" ", " ", " "), Array(" ", " ", " "), Array(" ", " ", " "))
-      whoseTurn = "O"
-      winner = " "
-      gameOver = false
-      }
+  //this function makes a player's move
+  def playerMove(turn: String, grid: Array[Array[String]]): Unit = {
+    val coordinateInput = readLine(s"Where would you like to place your $turn? ")
+    val coordinate = coordinateInput.split(",").map(_.toInt)
 
-    //This function prints out instructions at the beginning
-    def printInstructions(): Unit = {
-      println("INSTRUCTIONS:")
-      println("The grid is of the form")
-      println("|-----|-----|-----|")
-      for (i <- 0 to 2; j <- 0 to 2) {
-        if (j == 0) {
-          print("|")
-        }
-        print(s"($i,$j)")
-        if (j == 2) {
-          println("|")
-          println("|-----|-----|-----|")
-        }
-        else {
-          print("|")
-        }
-      }
-      println("To place your symbol, you must enter the coordinate where you wish to place your symbol.")
-      println("Input must be given in the form x,y with no space or brackets.")
-      println("Example: Where would you like to place your O? 1,1")
-      println("In case of incorrect placement, the system will ask you to try again.")
-      println("That's all. Have fun!")
-      println()
-      println("GAME BEGINS")
-    }
-
-    //this function prints the grid after every move
-    def printGrid(grid: Array[Array[String]]): Unit = {
-      println("|---|---|---|")
-      for (i <- 0 to 2; j <- 0 to 2) {
-        if (j == 0) {
-          print("| ")
-        }
-        print(grid(i)(j))
-        if (j == 2) {
-          println(" | ")
-          println("|---|---|---|")
-        }
-        else {
-          print(" | ")
-        }
-      }
-    }
-
-    //this function makes a player's move
-    def playerMove(turn: String, grid: Array[Array[String]]): Unit = {
-      val coordinateInput = readLine(s"Where would you like to place your $turn? ")
-//        coordinateInput match {
-//            case "1" => "0,0"
-//            case "2" => "0,1"
-//            case "3" => "0,2"
-//            case "4" => "1,0"
-//            case "5" => "1,1"
-//            case "6" => "1,2"
-//            case "7" => "2,0"
-//            case "8" => "2,1"
-//            case "9" => "2,2"
-////            case _  => println("Error input, please enter numbers from 0 to 2.")
-//            }
-      val coordinate = coordinateInput.split(",").map(_.toInt)
+    if ((((coordinate(0) < 0) || (coordinate(0) > 2)) &&
+      ((coordinate(1) < 0) || (coordinate(1) > 2))) ||
+      (((coordinate(0) < 0) || (coordinate(0) > 2)) &&
+      ((coordinate(1) >= 0) && (coordinate(1) <= 2))) ||
+      (((coordinate(0) >= 0) && (coordinate(0) <= 2)) &&
+      ((coordinate(1) < 0) || (coordinate(1) > 2)))) {
+      println("Oops! no - no - no.  Try again!")
+      playerMove(turn, grid)
+    } else {
       if (grid(coordinate(0))(coordinate(1)) == " ") {
         grid(coordinate(0))(coordinate(1)) = turn
         switchTurn()
@@ -207,4 +207,5 @@ object Final2 extends App {
       gameOver = true
     }
 
+  }
 }
